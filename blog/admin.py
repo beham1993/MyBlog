3b0dc348cd0595a7ef4df3e02fb2ccd3eee3f7f0
beham1993 <beham1993@gmail.com>
2016-03-04 21:53:04 +0200

    step 1 finish

--- blog/admin.py
+++ blog/admin.py
@@ -4,7 +4,7 @@ from blog.models import Article, Person, Quote
 class ArticleAdmin(admin.ModelAdmin):
 	fields = ['head_uk', 'head_en', 'sub_head_uk', 'sub_head_en', 'date_of_article', 'article_image', 'text_article_uk', 'text_article_en'],
 	list_display = ('head_uk', 'head_en', 'sub_head_uk', 'sub_head_en', 'article_image', "date_of_article")
-
+	
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Person)
