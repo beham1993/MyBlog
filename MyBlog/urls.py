a8739a48b5c61af79d1c152df244daf45dbc38a7
beham1993 <beham1993@gmail.com>
2016-03-12 23:21:32 +0200

    authorization try2

--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^i18n/', include('django.conf.urls.i18n')),
-    url(r'', include('social_auth.urls')),
     url(r'', include('blog.urls')),
+    url('', include('social.apps.django_app.urls', namespace='social')),
+    url('', include('django.contrib.auth.urls', namespace='auth')),
 )
