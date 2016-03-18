26ef612929f034ebddef0920f0fc0175895be58a
beham1993 <beham1993@gmail.com>
2016-03-18 16:08:43 +0200

    add dependence

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView, TemplateView
 from blog.models import Article, Person, Quote
+from django.contrib.auth import logout as auth_logout
 import random
 
 
